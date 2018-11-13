13a131ede5cdd330905930d3cde83a05ed1d171d
Jirat Thongpeng <42910435+it61070027@users.noreply.github.com>
2018-11-13 16:44:03 +0700

    13/11/2561

---
+++ tw.py
@@ -0,0 +1,17 @@
+""" Project Psit 
+    suicide analysis
+    thai suicide """
+import pandas, numpy, pygal
+def sum_thai():
+    """ sum number suicide thai 20 year """
+    df = pandas.read_csv('thai_suicide.csv')
+    data = numpy.array(df.groupby('year').sum()[['suicides_no', 'population']]).tolist()
+    chart = pygal.Line()
+    chart.title = 'Thailand suicide rate in 20 years'
+    chart.x_labels = map(str, range(1995, 2016))
+    chart.add('Thai Suicide', [i[0] for i in data])
+    chart.render_to_file('thai.svg')
+
+
+sum_thai()
+
