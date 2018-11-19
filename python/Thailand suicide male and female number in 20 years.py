a92c55173c1f7843b4e13185f5152f7350068cbf
Krittima Chantachalee <42972920+ckrittima@users.noreply.github.com>
2018-11-19 23:19:12 +0700

    19/11/2561

--- python/Thailand suicide male and female number in 20 years.py
+++ python/Thailand suicide male and female number in 20 years.py
@@ -17,12 +17,12 @@ def sum_sex():
     data_female_sum_year = numpy.array(data_female.groupby('year').sum()['suicides_no']).tolist()
     data_male_sum_year = numpy.array(data_male.groupby('year').sum()['suicides_no']).tolist()
     chart = pygal.StackedBar(style=custom_style)
-    chart.title = 'Thailand suicide  male and female rate in 20 years'
+    chart.title = 'Thailand suicide male and female number in 20 years'
     chart.x_title = 'Year'
     chart.y_title = 'Number Of Suicides'
     chart.x_labels = map(str, range(1995, 2016))
     chart.legend_at_bottom = True
     chart.add('Male', data_male_sum_year)
     chart.add('Female', data_female_sum_year)
-    chart.render_to_file('thai_rate_suicide_male_female.svg')
+    chart.render_to_file('thai_number_suicide_male_female.svg')
 sum_sex()
