66b8da73a7153f49ee3660453af73685ed2ac23e
Jirat Thongpeng <42910435+it61070027@users.noreply.github.com>
2018-11-27 15:48:29 +0700

    23/21

--- python/Number of suicide (age only).py
+++ python/Number of suicide (age only).py
@@ -4,7 +4,7 @@
 import pandas, numpy, pygal
 from pygal.style import Style
 def age():
-    custom_style = Style(colors=('#ff0000', '#0000fe','#ff0081', '#fff381', '#a6f381', '#741336'))
+    custom_style = Style(colors=('#ff0000', '#0000fe','#ff0081', '#1ac3f8', '#61b700', '#ffb700'))
     df = pandas.read_csv('thai_suicide.csv')
     data_age = numpy.array(df.groupby('age').sum()['suicides_no']).tolist()
     dt_age = ['15-24 years', '25-34 years', '35-54 years', '5-14 years', '55-74 years', '75+ years']
